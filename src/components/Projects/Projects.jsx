1db5418af119796d6ccccb490576050ddee8e588
Daniel Kim <danielkim1345@gmail.com>
2022-06-22 22:48:03 -0400

    added projects section

---
+++ src/components/Projects/Projects.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Flicking, { MoveEvent, WillChangeEvent } from "@egjs/react-flicking";
+
+
+const Projects = () => {
+    return (
+      <section id='projects' style={{height: "100vh"}}> 
+
+
+
+      </section>
+    )
+}
+
+export default Projects
