91a88843ff7a92587464b81ebfa86542b8537e0c
Daniel Kim <danielkim1345@gmail.com>
2022-06-22 22:48:03 -0400

    added projects section

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import {BrowserRouter as Router} from 'react-router-dom'
 import Welcome from './pages/welcome';
 import About from './components/About/About';
+import Projects from './components/Projects/Projects';
 import Experience from './components/Experience/Experience';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Welcome />
       <About />
       <Experience />
+      <Projects />
     </Router>
   );
 }
