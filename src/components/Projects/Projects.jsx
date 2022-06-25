dc7731d9b51006aa37fb3e640d4ae3e1518b490c
Daniel Kim <danielkim1345@gmail.com>
2022-06-25 02:26:49 -0400

    made changes to projects section

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,15 +1,21 @@
-import React from 'react'
-import Flicking, { MoveEvent, WillChangeEvent } from "@egjs/react-flicking";
-
-
-const Projects = () => {
-    return (
-      <section id='projects' style={{height: "100vh"}}> 
-
-
-
-      </section>
-    )
+import React from 'react';
+import './Projects.css';
+import { motion } from 'framer-motion';
+import {useState} from 'react';
+import { Box, Image, Badge, Text, Stack, useColorMode, Button, Flex, Spacer } from "@chakra-ui/react";
+import ManuLogo from "../../images/ManuLogo.jpg";
+import GlobalSpark from "../../images/GlobalSpark.png";
+import CSClub from "../../images/csClub.jpg"
+import AALogo from "../../images/AALogo.jpg"
+import UWLogo from "../../images/WlooLogo.png"
+  
+  
+const Projects = ({ data }) => {
+	return (
+		<section id="projects"> 
+			
+		</section>
+	);
 }
 
 export default Projects
