8ee1a79e31d05f78cafbd814b4dbe6afec451910
Daniel Kim <danielkim1345@gmail.com>
2022-06-26 00:31:59 -0400

    added changes

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Footer from './components/Footer/Footer';
 
 function App() {
   return (
-    <Router>
+    <Router basename='{process.env..PUBLIC_URL}'>
       <Welcome />
       <About />
       <Experience />
